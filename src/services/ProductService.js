f78c3dca43b2bfdcf7876a9f82a813ba1d61714b
Sbryan20 <91134367+Sbryan20@users.noreply.github.com>
2022-09-05 20:52:56 -0500

    gh-pages agregado

--- src/services/ProductService.js
+++ src/services/ProductService.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export class ProductService {
 
-     baseUrl = "http://localhost:8080/api/products/";
+     baseUrl = "https://bryant-rest-api.herokuapp.com/api/products/git";
    
     
 
